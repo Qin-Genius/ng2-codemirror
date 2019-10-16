dcc62fd9886daf226460b67c32535884d4fd359e
MaxSelena <923739313@qq.com>
2019-10-16 09:56:05 +0800

    commit web-sql

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import 'ng2-codemirror/node_modules/codemirror/lib/codemirror.js';
+import 'ng2-codemirror/node_modules/codemirror/mode/sql/sql.js';
+import 'ng2-codemirror/node_modules/codemirror/addon/hint/show-hint.js';
+import 'ng2-codemirror/node_modules/codemirror/addon/hint/sql-hint.js';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'web-sql';
+export class AppComponent implements OnInit {
+  config: any = {};
+  ngOnInit(): void {
+    this.config = { // codemirror组件的配置项
+      lineNumbers: true,                     //显示行号
+      mode: {name: 'text/x-mysql'},          //定义mode
+      // theme: 'ambiance', //主题
+      lineWrapping: true,
+      styleActiveLine: true,
+      matchBrackets: true,
+      autoRefresh: true,
+      dragDrop: true,
+      // extraKeys: {
+      //   "Ctrl": "autocomplete",
+      //   Tab: function (cm) {
+      //     const spaces = Array(cm.getOption('indentUnit') + 1).join(' ');
+      //     cm.replaceSelection(spaces);
+      //   }
+      // },   //自动提示配置
+    };
+  }
+
+  onCursorActivity($event) {
+    document.addEventListener('keypress', e => {
+      if (e.keyCode == 8) {
+        $event.instance.showHint(false);
+      } else {
+        $event.instance.showHint(true);
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+import {CodemirrorModule} from 'ng2-codemirror';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CodemirrorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
